d7ae300bef92e857b39ea9f96143b389ab80f416
Mr-Klaatu <mr_klaatu@me.com>
2014-08-26 09:53:45 +0300

    Initial Upload
    Add Cookie Consent warning to your Bolt site to comply with EU
    regulations.

---
+++ extension.php
@@ -0,0 +1,74 @@
+<?php
+// Cookie Consent extension for Bolt
+
+namespace CookieConsent;
+
+use Bolt\Extensions\Snippets\Location as SnippetLocation;
+
+class Extension extends \Bolt\BaseExtension
+{
+
+    function info() {
+
+        $data = array(
+            'name' =>"Cookie Consent",
+            'description' => "A small extension to add the European Cookie Consent Script to your site.",
+            'author' => "Mr Klaatu",
+            'link' => "http://santolife.com",
+            'version' => "0.1",
+            'required_bolt_version' => "1.0",
+            'highest_bolt_version' => "2.0",
+            'type' => "Snippet",
+            'first_releasedate' => "2014-09-01",
+            'latest_releasedate' => "2014-09-01",
+        );
+
+        return $data;
+
+    }
+
+    function initialize() {
+
+        // Add javascript file...
+        $this->addJavascript("assets/jquery.cookiesdirective.min.js");
+
+        // Add the consent script...
+        $this->addSnippet(SnippetLocation::END_OF_BODY, 'insertConsent');
+    }
+
+
+    function insertConsent()
+    {
+
+        $html = <<< EOM
+
+	<script type="text/javascript">
+        $(document).ready(function(){
+
+            $.cookiesDirective({
+                privacyPolicyUri: '%privacyUri%',
+                explicitConsent: %explicitConsent%,
+                position : '%cookieLocation%',
+                cookieScripts: '%cookieScripts%',
+                backgroundColor: '%backgroundColor%',
+                linkColor: '%linkColor%',
+                duration: %showTime%
+            });
+        });
+	</script>
+EOM;
+
+        $html = str_replace("%privacyUri%", $this->config['privacyUri']?$this->config['privacyUri']:'#', $html);
+        $html = str_replace("%explicitConsent%", $this->config['explicitConsent']?$this->config['explicitConsent']:'false', $html);
+        $html = str_replace("%cookieLocation%", $this->config['cookieLocation']?$this->config['cookieLocation']:'top', $html);
+        $html = str_replace("%cookieScripts%", $this->config['cookieScripts']?$this->config['cookieScripts']:'none', $html);
+        $html = str_replace("%backgroundColor%", $this->config['backgroundColor']?$this->config['backgroundColor']:'#52b54a', $html);
+        $html = str_replace("%linkColor%", $this->config['linkColor']?$this->config['linkColor']:'#ffffff', $html);
+        $html = str_replace("%showTime%", $this->config['showTime']?$this->config['showTime']:10, $html);
+
+
+        return new \Twig_Markup($html, 'UTF-8');
+
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+Cookie Consent
+==============
+This extension inserts the Cookie Consent extension into your pages.
+Edit the `config.yml` to configure the extension.
+
+Credit
+======
+This extension uses the Cookies Directive script by Ollie Philips.
+All credit to him for this great piece of work.
+You can get more information on the javascript used in this extension by going to http://cookiesdirective.com
+
